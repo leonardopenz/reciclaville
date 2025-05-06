5b83b8169b817d7334f30bd08c63e74341ff1a6f
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-05-06 15:55:23 -0300

    Configurado o ambiente no Spring Initializr

---
+++ src/main/java/br/com/senai/reciclaville/ReciclavilleApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.reciclaville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclavilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclavilleApplication.class, args);
+	}
+
+}
