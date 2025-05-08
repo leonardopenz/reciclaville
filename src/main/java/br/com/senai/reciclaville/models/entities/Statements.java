db4b40bf8f14928f64879a93da779a6f58698db6
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-05-07 22:23:54 -0300

    Criado a entidade Declaracao

--- src/main/java/br/com/senai/reciclaville/models/entities/Statements.java
+++ src/main/java/br/com/senai/reciclaville/models/entities/Statements.java
@@ -1,4 +1,42 @@
 package br.com.senai.reciclaville.models.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "declaracoes")
 public class Statements {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private String cliente;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate dataDeclaracao;
+
+    @Column(nullable = false)
+    private LocalDate dataInicial;
+
+    @Column(nullable = false)
+    private LocalDate dataFinal;
+
+    @Column(nullable = false)
+    private BigDecimal totalMateriais;
+
+    @Column(nullable = false)
+    private BigDecimal totalCompensacao;
 }
