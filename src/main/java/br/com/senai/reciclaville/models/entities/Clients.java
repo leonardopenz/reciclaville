5589c366e125b1768aa5c3e9c8adadb9edf2dc2b
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-05-07 21:10:19 -0300

    Criada entidade Cliente

--- src/main/java/br/com/senai/reciclaville/models/entities/Clients.java
+++ src/main/java/br/com/senai/reciclaville/models/entities/Clients.java
@@ -1,4 +1,26 @@
 package br.com.senai.reciclaville.models.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clientes")
 public class Clients {
+
+    private Long id;
+
+    private String nome;
+
+    private String cnpj;
+
+    private String atividadeEconomica;
+
+    private String responsavel;
+
 }
