f6f668e46c9aac60115d114451003e72cbddbcb6
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-05-07 21:42:05 -0300

    Criada a entidade Materiais

--- src/main/java/br/com/senai/reciclaville/models/entities/Materials.java
+++ src/main/java/br/com/senai/reciclaville/models/entities/Materials.java
@@ -1,4 +1,24 @@
 package br.com.senai.reciclaville.models.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "materiais")
 public class Materials {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer percentualCompensacao;
 }
